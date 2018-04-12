67316242c9ce9f8c4f6dd0e1ab9f1f898d9f882e
Divikiran Ravela <divikiran1@gmail.com>
2018-04-12 22:54:20 +1000

    First commit

---
+++ BlurEffect1/BlurEffect1/BlurEffect1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BlurEffect1
+{
+    public partial class BlurEffect1Page : ContentPage
+    {
+        public BlurEffect1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
